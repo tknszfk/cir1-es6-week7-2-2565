a204a71b97546d354e0c723f65b8e8c5a7103422
tknsafk <cdadwngs@gmail.com>
2022-11-28 09:32:38 +0700

    update

--- week702.js
+++ week702.js
@@ -1,5 +1,5 @@
-function add(a,b) { //define function add()
-    let c = a + b
+function add(a,b,x) { //define function add()
+    let c = a + b + x + y
     console.log(c)
 }
-add(5,3)//calling function
+add(5,3,10)//calling function
